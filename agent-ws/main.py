4ef427c71eca3f7bcc1edef60124f3a8e62dbb03
whs31 <75864292+whs31@users.noreply.github.com>
2023-07-13 14:03:05 +0300

    Battery status

--- agent-ws/main.py
+++ agent-ws/main.py
@@ -1,5 +1,24 @@
 import psutil
-import termcolor
+from termcolor import colored, cprint
+
+battery_percent = 0
+battery_charging = False
+
+
+def fetch_data():
+    global battery_percent
+    global battery_charging
+
+    battery_percent = psutil.sensors_battery().percent
+    battery_charging = psutil.sensors_battery().power_plugged
+
+
+def print_data():
+    cprint("SYSTEM STATUS", "black", "on_light_grey", attrs=["bold"])
+    charge_status = 'charging' if battery_charging else 'discharging'
+    print(f'🗲 Battery: {battery_percent}%, {charge_status}')
+
 
 if __name__ == '__main__':
-    print('🗲 Ba', psutil.sensors_battery())
+    fetch_data()
+    print_data()
