00091562705c8db32661769b0111ba66814ec09c
whs31 <75864292+whs31@users.noreply.github.com>
2023-07-13 15:10:47 +0300

    CPU info

--- agent-ws/main.py
+++ agent-ws/main.py
@@ -19,17 +19,25 @@ p_cpu_info = cpuinfo.get_cpu_info()
 cpu_model = p_cpu_info['brand_raw']
 cpu_freq = p_cpu_info['hz_advertised_friendly']
 cpu_arch = p_cpu_info['arch_string_raw']
+cpu_count = psutil.cpu_count()
+cpu_load_avg = 0
+cpu_load_per_core = 0
 
 
 def fetch_data():
     global battery_percent
     global battery_charging
     global os_boot_time
+    global cpu_load_avg
+    global cpu_load_per_core
 
     battery_percent = psutil.sensors_battery().percent
     battery_charging = psutil.sensors_battery().power_plugged
 
     os_boot_time = psutil.boot_time()
+
+    cpu_load_avg = psutil.cpu_percent(interval=0.5, percpu=False)
+    cpu_load_per_core = psutil.cpu_percent(interval=0.5, percpu=True)
 
 
 def print_data():
@@ -51,14 +59,18 @@ def print_data():
     print(f'Model:\t\t {cpu_model}')
     print(f'Architecture:\t {cpu_arch}')
     print(f'Frequency:\t {cpu_freq}')
+    print(f'Cores:\t\t {cpu_count}')
+    print('\n')
+    print(f'Average load:\t {cpu_load_avg}%')
+    print(f'Load per core:\t {cpu_load_per_core}')
     print('\n')
 
 
 if __name__ == '__main__':
     while True:
-        os.system('cls' if os.name == 'nt' else 'clear')
-
         fetch_data()
+        os.system('cls' if os.name == 'nt' else 'clear')
         print_data()
 
-        time.sleep(1)
+        #time.sleep(1)
+
