2e5e65fad93dab1115792fb2156d29bb883312d8
whs31 <75864292+whs31@users.noreply.github.com>
2023-07-15 20:50:43 +0300

    Basic GUI setup

---
+++ gui/src/c++/m_sys_monitor.c++
@@ -0,0 +1,12 @@
+/**
+  *  @file m_sys_monitor.c++
+  *  @author Dmitry Ryazancev
+  *  @date 15.07.23
+  *  @copyright Radar-MMS 2023
+  */
+
+#include "m_sys_monitor.h"
+
+namespace Application
+{
+} // Application
---
+++ gui/src/c++/m_sys_monitor.h
@@ -0,0 +1,16 @@
+/**
+  *  @file m_sys_monitor.h
+  *  @author Dmitry Ryazancev
+  *  @date 15.07.23
+  *  @copyright Radar-MMS 2023
+  */
+
+#pragma once
+
+namespace Application
+{
+  class MSysMonitor
+  {
+
+  };
+} // Application
